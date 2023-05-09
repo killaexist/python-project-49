e66c61f99570027756324b4cc0c74edc232c8aaf
Alexandr Lakhnenko <sashaolegovitchz@mail.ru>
2023-05-09 23:46:40 +0300

    chnge calc_logic/.toml

--- brain_games/games/calc_logic.py
+++ brain_games/games/calc_logic.py
@@ -15,8 +15,35 @@ def is_calc():
          r_exprssn = choice(exprssn)
          print(f'Question: {r_num} {r_exprssn} {r_num2}')
          ans = prompt.string('Your answer: ')
-         if (r_num int(r_exprssn) r_num2) == int(ans):
-              count_correct += 1
-              print('Correct!')
-
+         if r_exprssn == '+':
+             pluse = r_num + r_num2
+             if pluse == int(ans):
+                 print('Correct!')
+                 count_correct += 1
+             else:
+                 print(f"'{ans}' is wrong answer ;(. Correct answer was '{pluse}'")
+                 print(f"Let's try again, {temp}!")
+                 break
+         
+         elif r_exprssn == '-':
+             minus = r_num - r_num2
+             if minus == int(ans):
+                 print('Correct!')
+                 count_correct += 1
+             else:
+                 print(f"'{ans}' is wrong answer ;(. Correct answer was '{minus}'")
+                 print(f"Let's try again, {temp}!")
+                 break
+         
+         elif r_exprssn == '*':
+             multiply = r_num * r_num2
+             if multiply == int(ans):
+                 print('Correct!')
+                 count_correct += 1
+             else:
+                 print(f"'{ans}' is wrong answer ;(. Correct answer was '{multiply}'")
+                 print(f"Let's try again, {temp}!")
+                 break
+    if count_correct == 3:
+        print(f'Congratulations, {temp}')
 
