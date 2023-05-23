418c3d29c1ca1a9f2d017e1df3e1e2caf25b5d64
Alexandr Lakhnenko <sashaolegovitchz@mail.ru>
2023-05-23 22:41:45 +0300

    correct prgrssn_logic [linter]

--- brain_games/games/prgrssn_logic.py
+++ brain_games/games/prgrssn_logic.py
@@ -1,10 +1,11 @@
 import prompt
 from random import randint
-from brain_games.games.template import wrong_answer, is_greeting, progression_desc
+from brain_games.games.template import wrong_ans, is_greeting, prgrssn_desc
+
 
 def is_progression():
     name = is_greeting()
-    progression_desc()
+    prgrssn_desc()
     count_correct = 0
     list_num = []
     list_str = ''
@@ -27,13 +28,8 @@ def is_progression():
             list_str = ''
             list_num = []
         else:
-            print(f"'{ans}' {wrong_answer()} '{list_num[hide_index - 1] + step}'")
+            print(f"'{ans}' {wrong_ans()} '{list_num[hide_index - 1] + step}'")
             print(f"Let's try again, {name}!")
             break
     if count_correct == 3:
         print(f'Congratulations, {name}!')
-
-
-
-
-
